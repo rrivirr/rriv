c91f8896f913c18ff28c3a179f8d12dd0fc93ab5
Matthew Xi <matt@winterroot.net>
2017-10-23 21:25:49 -0700

    WIP filter files by time, communicate last download time to the android

--- src/aquarius-arduino.cpp
+++ src/aquarius-arduino.cpp
@@ -19,13 +19,6 @@ const int chipSelect = 10;
 // File system object.
 SdFat sd;
 File logfile;
-File settingsFile;
-
-// Settings
-int device_id = 0;
-int sensor_1 = 0;
-int sensor_2 = 0;
-int sensor_3 = 0;
 
 int state = 0;
 
@@ -41,25 +34,36 @@ char lastDownloadDate[11] = "0000000000";
 *
 */
 
-String getNewFilname(){
-  DateTime now = RTC.now();
-  String uniquename = String(now.unixtime(), DEC);
-  String suffix = ".CSV";
-  String filename = "";
-  filename.concat(uniquename);
-  filename.concat(suffix);
-  return filename;
-}
 
-void setDataFile(){
-  String filename = getNewFilname();
+void setNewDataFile(){
+  DateTime now = RTC.now();
+  char uniquename[11];
+  sprintf(uniquename, "%lu", now.unixtime());
+  char suffix[5] = ".CSV";
+  char filename[15];
+  strncpy(filename, uniquename, 10);
+  strncpy(&filename[10], suffix, 5);
+
   Serial.print("Logging to: ");
   Serial.println(filename);
-  logfile = sd.open("Data/" + filename, FILE_WRITE);
+
+  //d.chdir("Data", true);
+  logfile = sd.open(filename, FILE_WRITE);
+  //sd.chdir();
   if(!logfile){
-    Serial.println("file.open");
+    Serial.println("file did not open");
     while(1);
   }
+}
+
+void dateTime(uint16_t* date, uint16_t* time) {
+  DateTime now = RTC.now();
+
+  // return date using FAT_DATE macro to format fields
+  *date = FAT_DATE(now.year(), now.month(), now.day());
+
+  // return time using FAT_TIME macro to format fields
+  *time = FAT_TIME(now.hour(), now.minute(), now.second());
 }
 
 int uniqueIdAddressStart = 0;
@@ -80,14 +84,13 @@ void readUniqueId(){
   Serial.println("");
 
   unsigned char uninitializedEEPROM[16] = { 0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF,0xFF};
-  Serial.println("Here's the uninitializedEEPROM string that should be");
+  //erial.println("Here's the uninitializedEEPROM string that should be");
   for(int i=0; i<8; i++){
     Serial.print((unsigned int) uninitializedEEPROM[2*i], HEX);
   }
-  Serial.println("");
 
   if(memcmp(uuid, uninitializedEEPROM, 16) == 0){
-    Serial.println("Generate a UUID");
+    Serial.println("Generate UUID");
     // generate the unique ID
     TrueRandomClass::uuid(uuid);
     for(int i=0; i <= uniqueIdAddressEnd - uniqueIdAddressStart; i++){
@@ -101,6 +104,13 @@ void readUniqueId(){
 
 void initializeSDCard(void) {
   Wire.begin();
+
+  if (! RTC.initialized()) {
+    Serial.println("RTC is NOT running!");
+    // following line sets the RTC to the date & time this sketch was compiled
+    RTC.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
   if (!RTC.begin()) {
     #if ECHO_TO_SERIAL
     Serial.println("RTC failed");
@@ -110,6 +120,7 @@ void initializeSDCard(void) {
     //Serial.println(RTC.now().m);
   }
 
+  SdFile::dateTimeCallback(dateTime);
 
   // initialize the SD card
   Serial.print("Initializing SD card...");
@@ -123,12 +134,10 @@ void initializeSDCard(void) {
   }
   Serial.println("card initialized.");
 
-  setDataFile();
+  setNewDataFile();
 
   logfile.println("time,data1,data2,data3,data4,data5,data6,data7,data8");
-  #if ECHO_TO_SERIAL
-  Serial.println("time,data1,data2,data3,data4,data5,data6,data7,data8");
-  #endif //ECHO_TO_SERIAL// attempt to write out the header to the file
+
 
 }
 
@@ -143,7 +152,6 @@ void setup(void)
 
   Serial.begin(115200);
   //Serial.begin(9600);
-  Serial.println("Light Sensor Test"); Serial.println("");
 
   readUniqueId();
 
@@ -151,6 +159,24 @@ void setup(void)
   initializeSDCard();
 
   /* We're ready to go! */
+  Serial.println("done with setup");
+
+}
+
+void printCurrentDirListing(){
+  sd.vwd()->rewind();
+  SdFile dirFile;
+  char sdFilename[20];
+  while (dirFile.openNext(sd.vwd(), O_READ)) {
+    dir_t d;
+    if (!dirFile.dirEntry(&d)) {
+      Serial.println("f.dirEntry failed");
+      while(1);
+    }
+
+    dirFile.getName(sdFilename, 20);
+    Serial.println(sdFilename);
+  }
 
 }
 
@@ -161,8 +187,10 @@ should go here)
 */
 /**************************************************************************/
 uint32_t lastTime = 0;
+const int maxRequestLength = 34;
 void loop(void)
 {
+  Serial.println("Loop");
   DateTime now;
 
   // Just trigger the dump event using a basic button
@@ -170,10 +198,20 @@ void loop(void)
 
   if(Serial.peek() == '>' && state == 0){
     //Serial.println("Peek");
-    char request[34] = "";
-    Serial.readBytesUntil('<', request, 34);
-    //Serial.println(request);
-    if(strncmp(request, ">WT_REQUEST_DOWNLOAD",20) == 0) {
+
+    char request[maxRequestLength] = "";
+    Serial.readBytesUntil('<', request, maxRequestLength);
+    Serial.println(request);
+    if(strncmp(request, ">WT_OPEN_CONNECTION", 19) == 0) {
+      Serial.write(">WT_IDENTIFY_DEVICE:");
+      for(int i=0; i<8; i++){
+        Serial.print((unsigned int) uuid[2*i], HEX);
+      }
+      Serial.write("<");
+      Serial.flush();
+
+    }
+    else if(strncmp(request, ">WT_REQUEST_DOWNLOAD",20) == 0) {
       // Flush the input, would be better to use a delimiter
       // May not be necessary now
       unsigned long now = millis ();
@@ -189,6 +227,8 @@ void loop(void)
       Serial.flush();
       state = 1;
       return;
+    } else {
+      strcpy(lastDownloadDate, lastDownloadDateEmpty);
     }
 
   } else if(state == 1){
@@ -210,33 +250,30 @@ void loop(void)
     }
 
 
-    // Get list of files
+    // Debug
+    printCurrentDirListing();
 
-    Serial.println(lastDownloadDate);
-
-    //SdFile dataDirectory;
-    //dirFile.open("Data");
-
-    if (!sd.chdir("Data")) {
-       Serial.println("chdir failed for Data.\n");
+    if (!sd.chdir("/Data")) {
+      Serial.println("chdir failed for Data.\n");
+      state = 0;
+      return;
     }
+
+    // Debug
+    printCurrentDirListing();
+
+
     sd.vwd()->rewind();
     SdFile dirFile;
+    char sdFilename[20];
     while (dirFile.openNext(sd.vwd(), O_READ)) {
       dir_t d;
       if (!dirFile.dirEntry(&d)) {
         Serial.println("f.dirEntry failed");
         while(1);
       }
-      char sdFilename[20];
-      dirFile.getName(sdFilename, 20);
 
-      /*
-      Serial.print(sdFilename);
-      Serial.print(" ");
-      Serial.print(strncmp(sdFilename, date, 10) );
-      Serial.println();
-      */
+      dirFile.getName(sdFilename, 20);
 
       if(strncmp(sdFilename, lastDownloadDate, 10) > 0){
         //Serial.println(sdFilename);
@@ -253,209 +290,44 @@ void loop(void)
 
     }
     if (!sd.chdir()) {
-       Serial.println("chdir failed for ../\n");
+      Serial.println("chdir failed for ../\n");
+      state = 0;
+      return;
     }
-    strcpy(lastDownloadDateEmpty, lastDownloadDate);
+
+
+    setNewDataFile();
+    char transferCompleteMessage[34] = ">WT_TRANSFER_COMPLETE:0000000000<";
+    strncpy(&transferCompleteMessage[22], sdFilename, 10);
+    Serial.write(transferCompleteMessage);
+
+
+    // Send last download date to phone for book keeeping
     state = 0;
 
-
-    // So what is our strategy for dealing with files?
-    // Get the files past a certain date, I guess?
-    // But how would we compare this ?
-    // Filenames are gonna be date strings, not numbers
-    // and even so...
-
-    /*
-    File datafile = sd.open("DATALOG.TXT", FILE_WRITE);
-
-    // send size of transmission
-    // Serial.println(datafile.fileSize());
-    // return;
-
-    while (datafile.available()) {
-    Serial.write(datafile.read());
   }
-  datafile.close();
-
-  Serial.print("<"); // The byte to stop reading when we don't send total transfer size
-  Serial.flush();
-  state = 0;
-}
-*/
-} else {
-  //Serial.print("STATE_0");
-}
 
 
-// Fetch the time
-now = RTC.now();
-uint32_t elapsedTime = now.unixtime() - lastTime;
-if(elapsedTime < 5){
-  return;
-}
-lastTime = now.unixtime();
-
-
-// log uuid and time
-for(int i=0; i<8; i++){
-  logfile.print((unsigned int) uuid[2*i], HEX);
-}
-logfile.print(",");
-logfile.print(now.unixtime()); // seconds since 2000
-logfile.print(",");
-
-#if ECHO_TO_SERIAL
-Serial.print(now.unixtime()); // seconds since 2000
-Serial.print(",");
-/* Display the results (light is measured in lux) */
-
-
-/*Serial.print(now.year(), DEC);
-Serial.print("/");
-Serial.print(now.month(), DEC);
-Serial.print("/");
-Serial.print(now.day(), DEC);
-Serial.print(" ");
-Serial.print(now.hour(), DEC);
-Serial.print(":");
-Serial.print(now.minute(), DEC);
-Serial.print(":");
-Serial.print(now.second(), DEC);
-Serial.println();
-*/
-#endif
-
-logfile.println();
-logfile.flush();
-
-delay(250);
-
-}
-
-
-/* Apply the value to the parameter by searching for the parameter name
-Using String.toInt(); for Integers
-toFloat(string); for Float
-toBoolean(string); for Boolean
-toLong(string); for Long
-*/
-void applySetting(String settingName, String settingValue) {
-  if(settingName == "device_id") {
-    device_id=settingValue.toInt();
+  // Fetch the time
+  now = RTC.now();
+  uint32_t elapsedTime = now.unixtime() - lastTime;
+  if(elapsedTime < 5){
+    return;
   }
-  if(settingName == "sensor_1") {
-    sensor_1=settingValue.toInt();
+  lastTime = now.unixtime();
+
+
+  // log uuid and time
+  for(int i=0; i<8; i++){
+    logfile.print((unsigned int) uuid[2*i], HEX);
   }
-  if(settingName == "sensor_2") {
-    sensor_2=settingValue.toInt();
-  }
-  if(settingName == "sensor_3") {
-    sensor_3=settingValue.toInt();
-  }
-}
+  logfile.print(",");
+  logfile.print(now.unixtime()); // seconds since 2000
+  logfile.print(",");
 
+  logfile.println();
+  logfile.flush();
 
-// Saved settings should go into the EEPROM
-// not be saved here
+  delay(250);
 
-// Work with saved settings
-// from AlexShu http://overskill.alexshu.com/saving-loading-settings-on-sd-card-with-arduino/
-void readSDSettings(){
-  char character;
-  String settingName;
-  String settingValue;
-  settingsFile = sd.open("settings.txt");
-  if (settingsFile) {
-    while (settingsFile.available()) {
-      character = settingsFile.read();
-      while((settingsFile.available()) && (character != '[')){
-        character = settingsFile.read();
-      }
-      character = settingsFile.read();
-      while((settingsFile.available()) && (character != '=')){
-        settingName = settingName + character;
-        character = settingsFile.read();
-      }
-      character = settingsFile.read();
-      while((settingsFile.available()) && (character != ']')){
-        settingValue = settingValue + character;
-        character = settingsFile.read();
-      }
-      if(character == ']'){
-
-        /*
-        //Debuuging Printing
-        Serial.print("Name:");
-        Serial.println(settingName);
-        Serial.print("Value :");
-        Serial.println(settingValue);
-        */
-
-        // Apply the value to the parameter
-        applySetting(settingName,settingValue);
-        // Reset Strings
-        settingName = "";
-        settingValue = "";
-      }
-    }
-    // close the file:
-    settingsFile.close();
-  } else {
-    // if the file didn't open, print an error:
-    Serial.println("error opening settings.txt");
-  }
-}
-
-// converting string to Float
-float toFloat(String settingValue){
-  char floatbuf[settingValue.length()+1];
-  settingValue.toCharArray(floatbuf, sizeof(floatbuf));
-  float f = atof(floatbuf);
-  return f;
-}
-
-long toLong(String settingValue){
-  char longbuf[settingValue.length()+1];
-  settingValue.toCharArray(longbuf, sizeof(longbuf));
-  long l = atol(longbuf);
-  return l;
-}
-
-// Converting String to integer and then to boolean
-// 1 = true
-// 0 = false
-boolean toBoolean(String settingValue) {
-  if(settingValue.toInt()==1){
-    return true;
-  } else {
-    return false;
-  }
-}
-
-// Writes A Configuration file
-void writeSDSettings() {
-  // Delete the old One
-  sd.remove("settings.txt");
-  // Create new one
-  settingsFile = sd.open("settings.txt", FILE_WRITE);
-  // writing in the file works just like regular print()/println() function
-  settingsFile.print("[");
-  settingsFile.print("device_id=");
-  settingsFile.print(device_id);
-  settingsFile.println("]");
-  settingsFile.print("[");
-  settingsFile.print("sensor_1=");
-  settingsFile.print(sensor_1);
-  settingsFile.println("]");
-  settingsFile.print("[");
-  settingsFile.print("sensor_2=");
-  settingsFile.print(sensor_2);
-  settingsFile.println("]");
-  settingsFile.print("[");
-  settingsFile.print("sensor_3=");
-  settingsFile.print(sensor_3);
-  settingsFile.println("]");
-  // close the file:
-  settingsFile.close();
-  //Serial.println("Writing done.");
 }
