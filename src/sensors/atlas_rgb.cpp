6e2d36691971abef9e01fbe339bfc335822436d7
Shayne Marques <marques0@purdue.edu>
2021-06-07 10:44:19 -0400

    Switched to class definition

--- src/sensors/atlas_rgb.cpp
+++ src/sensors/atlas_rgb.cpp
@@ -1,2 +1,14 @@
 #include "atlas_rgb.h"
+#include <SoftwareSerial.h>
+
+void setupRGB(int rx, int tx) {
+  rgbSerial = SoftwareSerial(rx, tx);
+  inputString = "";
+  sensorString = "";
+  inputStringComplete = false;
+  
+}
+
+
+
 
