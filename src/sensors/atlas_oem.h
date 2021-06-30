d96ebc140cfd4a7763379cdf5c5f7ee0f4b642d8
Zaven Arra <zaven@waterbearsondes.io>
2021-06-30 16:07:11 -0700

    fix: switch to sensor driver interface for ec sensor

--- src/sensors/atlas_oem.h
+++ src/sensors/atlas_oem.h
@@ -1,15 +1,39 @@
-#ifndef WATERBEAR_ATLAS_OEM
-#define WATERBEAR_ATLAS_OEM
+#ifndef WATERBEAR_ATLAS_EC
+#define WATERBEAR_ATLAS_EC
 
-#include <Wire_slave.h> // Communicate with I2C/TWI devices
-#include <EC_OEM.h>
+#include "sensor.h"
 
-void setupEC_OEM(TwoWire * wire);
-void hibernateEC_OEM();
-void clearECCalibrationData();
-void setECDryPointCalibration();
-void setECLowPointCalibration(float lowPoint);
-void setECHighPointCalibration(float highPoint);
-bool readECDataIfAvailable(float * value);
+class AtlasEC : public I2CSensorDriver
+{
+
+  public: 
+    // Constructor
+    AtlasEC();
+
+    // Interface
+    void setup();
+    void stop();
+    bool takeMeasurement();
+    char * getDataString();
+    char * getCSVColumns();
+    protocol_type getProtocol();
+
+  private:
+    char csvColumnHeaders[100] = "ec";
+
+    char dataString[16];
+};
+
+
+// #include <Wire_slave.h> // Communicate with I2C/TWI devices
+// #include <EC_OEM.h>
+
+// void setupEC_OEM(TwoWire * wire);
+// void hibernateEC_OEM();
+// void clearECCalibrationData();
+// void setECDryPointCalibration();
+// void setECLowPointCalibration(float lowPoint);
+// void setECHighPointCalibration(float highPoint);
+// bool readECDataIfAvailable(float * value);
 
 #endif
