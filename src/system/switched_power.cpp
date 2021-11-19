44ae67cfe50f376690cd3960fa72f4fd8b04e43d
Your Name <jakehosen@gmail.com>
2021-11-19 09:19:40 -0500

    adding external MOSFET control at PC11

--- src/system/switched_power.cpp
+++ src/system/switched_power.cpp
@@ -5,6 +5,7 @@
 void setupSwitchedPower()
 {
   pinMode(SWITCHED_POWER_ENABLE, OUTPUT); // enable pin on switchable boost converter
+  pinMode(EXT_MOSFET_ENABLE, OUTPUT); // enable pin on switchable external MOSFET
   digitalWrite(SWITCHED_POWER_ENABLE, LOW);
 }
 
@@ -12,11 +13,15 @@ void enableSwitchedPower()
 {
   Monitor::instance()->writeDebugMessage(F("Enabling switched power"));
   digitalWrite(SWITCHED_POWER_ENABLE, HIGH);
+  delay(500);
+  digitalWrite(EXT_MOSFET_ENABLE, HIGH);
 }
 
 void disableSwitchedPower()
 {
   Monitor::instance()->writeDebugMessage(F("Disabling switched power"));
+  digitalWrite(EXT_MOSFET_ENABLE, LOW);
+  delay(500);
   digitalWrite(SWITCHED_POWER_ENABLE, LOW);
 }
 
